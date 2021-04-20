f86656e11856e416c1dbbc90c39d56a7e42da704
Tony <Jordybecerril@gmail.com>
2021-04-20 09:49:55 -0500

    vuegram project setup

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+
+
+// workaround for local host not working on port 8080.
+
+module.exports = {
+    devServer: {
+        // other config
+        port: 8081 // or any other port you wish to use other than 8080
+    }
+}
+
