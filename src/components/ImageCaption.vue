35b15ced2664be43c6009f110502b550ad0e9298
Jordi Stark <Jordybecerril@gmail.com>
2022-03-31 23:32:43 -0500

    commit to create .vuerc. adding vuetify3 lol

---
+++ src/components/ImageCaption.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h3>Chose a photo</h3>
+        <button class="button button--upload-btn">upload photo</button>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+          
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/views/Test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="col1">
+    <div class="container">
+      <h1>Test view</h1>
+      <hr />
+      <ImageCaption />
+    </div>
+  </div>
+</template>
+
+<script>
+import ImageCaption from "@/components/ImageCaption";
+
+export default {
+  components: {
+    ImageCaption,
+  },
+};
+</script>
+
+<style></style>
