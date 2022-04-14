4142336c3388caa09b507b7537e5b81c7c213a3d
Jordi Stark <Jordybecerril@gmail.com>
2022-04-14 14:21:29 -0500

    cleaning up project a bit

--- src/main.js
+++ src/main.js
@@ -8,8 +8,6 @@ import VueFinalModal from 'vue-final-modal'
 
 /* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
-<<<<<<< HEAD
-=======
 
 // /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -19,41 +17,17 @@ import { far } from '@fortawesome/free-regular-svg-icons'
 
 library.add(fas, far)
 
->>>>>>> 967dadd (adding images to posts. also can delete and edit posts.)
-
-// /* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { fas } from "@fortawesome/free-solid-svg-icons";
-import { far } from '@fortawesome/free-regular-svg-icons'
-
-
-library.add(fas, far)
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 9a16b57 (image file chosen, previewed and uploaded to the cloud (firestore))
 
 let app
 
   auth.onAuthStateChanged((user) => {
     if (!app) {
       app = createApp(App)
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 967dadd (adding images to posts. also can delete and edit posts.)
       app
       .component("font-awesome-icon", FontAwesomeIcon)
       .use(store)
       .use(router)
-      .use(VueFinalModal(), {
-<<<<<<< HEAD
-=======
       app.use(store).use(router).use(VueFinalModal(), {
->>>>>>> 9a16b57 (image file chosen, previewed and uploaded to the cloud (firestore))
-=======
->>>>>>> 967dadd (adding images to posts. also can delete and edit posts.)
         componentName: 'VueFinalModal',
         key: '$vfm',    
         dynamicContainerName: 'ModalsContainer'
