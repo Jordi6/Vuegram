cca518e633ac475aeacbc8be6bf395be3970b3ca
Jordi Stark <Jordybecerril@gmail.com>
2022-04-14 10:02:59 -0500

    adding images to posts. also can delete and edit posts.

--- src/main.js
+++ src/main.js
@@ -8,6 +8,18 @@ import VueFinalModal from 'vue-final-modal'
 
 /* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
+<<<<<<< HEAD
+=======
+
+// /* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from '@fortawesome/free-regular-svg-icons'
+
+
+library.add(fas, far)
+
+>>>>>>> 967dadd (adding images to posts. also can delete and edit posts.)
 
 // /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -28,14 +40,20 @@ let app
     if (!app) {
       app = createApp(App)
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 967dadd (adding images to posts. also can delete and edit posts.)
       app
       .component("font-awesome-icon", FontAwesomeIcon)
       .use(store)
       .use(router)
       .use(VueFinalModal(), {
+<<<<<<< HEAD
 =======
       app.use(store).use(router).use(VueFinalModal(), {
 >>>>>>> 9a16b57 (image file chosen, previewed and uploaded to the cloud (firestore))
+=======
+>>>>>>> 967dadd (adding images to posts. also can delete and edit posts.)
         componentName: 'VueFinalModal',
         key: '$vfm',    
         dynamicContainerName: 'ModalsContainer'
