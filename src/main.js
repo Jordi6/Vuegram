bdccc3ea62ea2703f90423b33bfd9f872e02eaca
Jordi6 <jordyBecerril@gmail.com>
2022-05-03 05:25:28 +0000

    adding docker compose

--- src/main.js
+++ src/main.js
@@ -4,15 +4,16 @@ import router from "./router";
 import store from "./store";
 import { auth } from "./firebase";
 import "./assets/SCSS/app.scss";
-import VueFinalModal from 'vue-final-modal'
+import VueFinalModal from 'vue-final-modal';
+import * as fb from "./firebase";
 
 /* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { library } from '@fortawesome/fontawesome-svg-core';
 
 // /* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { fas } from "@fortawesome/free-solid-svg-icons";
-import { far } from '@fortawesome/free-regular-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons';
 
 
 library.add(fas, far)
@@ -22,22 +23,22 @@ let app
 
   auth.onAuthStateChanged((user) => {
     if (!app) {
-      app = createApp(App)
+      app = createApp(App);
       app
-      .component("font-awesome-icon", FontAwesomeIcon)
       .use(store)
       .use(router)
-      app.use(store).use(router).use(VueFinalModal(), {
+      .component("font-awesome-icon", FontAwesomeIcon)
+      .use(VueFinalModal(), {
         componentName: 'VueFinalModal',
         key: '$vfm',    
         dynamicContainerName: 'ModalsContainer'
       })
       .mount('#app')
     }
+    
     if (user) {
-      store.dispatch('fetchUserProfile', user)
+      store.dispatch('fetchUserProfile', user);
+      store.dispatch('fetchPosts', user);
     }
-  })
-  
 
-  
+  });
